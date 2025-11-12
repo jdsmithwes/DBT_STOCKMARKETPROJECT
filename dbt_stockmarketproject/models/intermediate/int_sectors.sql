634ff351e00a366f76bb525c460d25ac8d4029d0
jdsmithwes <jdsmith1906@gmail.com>
2025-11-12 16:07:13 -0500

    int_sectors model added to dbt project

--- dbt_stockmarketproject/models/intermediate/int_sectors.sql
+++ dbt_stockmarketproject/models/intermediate/int_sectors.sql
@@ -1,35 +1,34 @@
 with context_info as (
     select
         "Industry",
-        Symbol,
-        MarketCapitalization,
-        EBITDA,
-        PERatio,
-        RevenueTTM,
-        GrossProfitTTM,
-        AnalystRatingStrongBuy,
-        AnalystRatingBuy,
-        AnalystRatingHold,
-        AnalystRatingSell,
-        AnalystRatingStrongSell
-
+        "SYMBOL",
+        "MarketCapitalization",
+        "EBITDA",
+        "PERatio",
+        "RevenueTTM",
+        "GrossProfitTTM",
+        "AnalystRatingStrongBuy",
+        "AnalystRatingBuy",
+        "AnalystRatingHold",
+        "AnalystRatingSell",
+        "AnalystRatingStrongSell"
     from {{ ref('stg_stockoverview') }}
     )
 
 select
     "Industry",
-    count(distinct Symbol) as NumberOfCompanies,
-    avg(MarketCapitalization) as AvgMarketCapitalization,
-    avg(EBITDA) as AvgEBITDA,
-    avg(PERatio) as AvgPERatio,
-    avg(RevenueTTM) as AvgRevenueTTM,
-    avg(GrossProfitTTM) as AvgGrossProfitTTM,
-    count(AnalystRatingStrongBuy) as TotalStrongBuy,
-    count(AnalystRatingBuy) as TotalBuy,
-    count(AnalystRatingHold) as TotalHold,
-    count(AnalystRatingSell) as TotalSell,
-    count(AnalystRatingStrongSell) as TotalStrongSell
+    count(distinct "SYMBOL") as NumberOfCompanies,
+    avg("MarketCapitalization") as AvgMarketCapitalization,
+    avg("EBITDA") as AvgEBITDA,
+    avg("PERatio") as AvgPERatio,
+    avg("RevenueTTM") as AvgRevenueTTM,
+    avg("GrossProfitTTM") as AvgGrossProfitTTM,
+    count("AnalystRatingStrongBuy") as TotalStrongBuy,
+    count("AnalystRatingBuy") as TotalBuy,
+    count("AnalystRatingHold") as TotalHold,
+    count("AnalystRatingSell") as TotalSell,
+    count("AnalystRatingStrongSell") as TotalStrongSell
 from context_info
-group by Industry
+group by "Industry"
 order by NumberOfCompanies desc
     
