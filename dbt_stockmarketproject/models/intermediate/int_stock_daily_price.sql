87168a3b6c1a3aa0ad1913695ec7322fe95a5ee6
jdsmithwes <jdsmith1906@gmail.com>
2025-11-16 07:42:36 -0500

    refactored parent directory name to JamaalDBTProject to avoid errors with folder that had DBTproject

--- dbt_stockmarketproject/models/intermediate/int_stock_daily_price.sql
+++ dbt_stockmarketproject/models/intermediate/int_stock_daily_price.sql
@@ -22,24 +22,21 @@ summary AS (
     SELECT
         stock_ticker,
 
-        -- first and last trading days
-        MIN(trading_date) AS first_trading_date,
-        MAX(trading_date) AS last_trading_date,
+        MIN(trading_date)            AS first_trading_date,
+        MAX(trading_date)            AS last_trading_date,
 
-        -- prices
-        AVG(close_price) AS avg_close_price,
-        MAX(interday_high_price) AS all_time_high,
-        MIN(interday_low_price)  AS all_time_low,
+        AVG(close_price)             AS avg_close_price,
+        MAX(interday_high_price)     AS all_time_high,
+        MIN(interday_low_price)      AS all_time_low,
 
-        -- volume
-        SUM(trading_volume) AS total_volume_traded,
-        MAX(trading_volume) AS max_daily_volume,
+        SUM(trading_volume)          AS total_volume_traded,
+        MAX(trading_volume)          AS max_daily_volume,
 
-        -- dividends
-        SUM(dividend_amount) AS total_dividends
+        SUM(dividend_amount)         AS total_dividends
 
     FROM base
     GROUP BY stock_ticker
 )
 
-SELECT * FROM summary;
+SELECT *
+FROM summary
