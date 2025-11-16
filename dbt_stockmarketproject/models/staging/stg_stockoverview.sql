69ff478236919af63f86cebd0a6173c4303b0003
jdsmithwes <jdsmith1906@gmail.com>
2025-11-16 11:43:33 -0500

    set up Snowpipe for company overview

--- dbt_stockmarketproject/models/staging/stg_stockoverview.sql
+++ dbt_stockmarketproject/models/staging/stg_stockoverview.sql
@@ -1,72 +1,69 @@
-WITH source_data AS (
+{{ config(materialized='view') }}
 
-    SELECT
-        -- base alpha vantage fields
-        symbol,
-        assettype,
-        name,
-        description,
-        cik,
-        exchange,
-        currency,
-        country,
-        sector,
-        industry,
-        address,
+with raw as (
+    select
+        SYMBOL,
+        ASSETTYPE,
+        NAME,
+        DESCRIPTION,
+        CIK,
+        EXCHANGE,
+        CURRENCY,
+        COUNTRY,
+        SECTOR,
+        INDUSTRY,
+        ADDRESS,
 
-        -- date strings -> cast to DATE
-        TRY_TO_DATE(fiscalyearend)         AS fiscal_year_end,
-        TRY_TO_DATE(latestquarter)         AS latest_quarter,
+        -- DATE CASTS
+        TRY_TO_DATE(FISCALYEAREND)        as fiscal_year_end,
+        TRY_TO_DATE(LATESTQUARTER)        as latest_quarter,
 
-        -- numeric casts
-        TRY_TO_NUMBER(marketcapitalization)    AS market_cap,
-        TRY_TO_NUMBER(ebitda)                  AS ebitda,
-        TRY_TO_NUMBER(peratio)                 AS pe_ratio,
-        TRY_TO_NUMBER(pegratio)                AS peg_ratio,
-        TRY_TO_NUMBER(bookvalue)               AS book_value,
-        TRY_TO_NUMBER(dividendpershare)        AS dividend_per_share,
-        TRY_TO_NUMBER(dividendyield)           AS dividend_yield,
-        TRY_TO_NUMBER(eps)                     AS eps,
-        TRY_TO_NUMBER(revenuepersharettm)      AS revenue_per_share_ttm,
-        TRY_TO_NUMBER(profitmargin)            AS profit_margin,
-        TRY_TO_NUMBER(operatingmarginttm)      AS operating_margin_ttm,
-        TRY_TO_NUMBER(returnonassets)          AS return_on_assets,
-        TRY_TO_NUMBER(returnonequity)          AS return_on_equity,
-        TRY_TO_NUMBER(revenueTTM)              AS revenue_ttm,
-        TRY_TO_NUMBER(grossProfitTTM)          AS gross_profit_ttm,
-        TRY_TO_NUMBER(dilutedEPSTTM)           AS diluted_eps_ttm,
-        TRY_TO_NUMBER(quarterlyEarningsGrowthYOY)    AS quarterly_earnings_growth_yoy,
-        TRY_TO_NUMBER(quarterlyRevenueGrowthYOY)     AS quarterly_revenue_growth_yoy,
-        TRY_TO_NUMBER(analystTargetPrice)      AS analyst_target_price,
-        TRY_TO_NUMBER(trailingPE)              AS trailing_pe,
-        TRY_TO_NUMBER(forwardPE)               AS forward_pe,
-        TRY_TO_NUMBER(priceToSalesTTM)         AS price_to_sales_ttm,
-        TRY_TO_NUMBER(priceToBookRatio)        AS price_to_book_ratio,
-        TRY_TO_NUMBER(evToRevenue)             AS ev_to_revenue,
-        TRY_TO_NUMBER(evToEBITDA)              AS ev_to_ebitda,
-        TRY_TO_NUMBER(beta)                    AS beta,
-        TRY_TO_NUMBER(week52high)              AS week_52_high,
-        TRY_TO_NUMBER(week52low)               AS week_52_low,
-        TRY_TO_NUMBER(day50movingaverage)      AS day_50_ma,
-        TRY_TO_NUMBER(day200movingaverage)     AS day_200_ma,
-        TRY_TO_NUMBER(sharesoutstanding)       AS shares_outstanding,
+        -- NUMERIC FIELDS
+        TRY_TO_NUMBER(MARKETCAPITALIZATION)   as market_cap,
+        TRY_TO_NUMBER(EBITDA)                 as ebitda,
+        TRY_TO_NUMBER(PERATIO)                as pe_ratio,
+        TRY_TO_NUMBER(PEGRATIO)               as peg_ratio,
+        TRY_TO_NUMBER(BOOKVALUE)              as book_value,
+        TRY_TO_NUMBER(DIVIDENDPERSHARE)       as dividend_per_share,
+        TRY_TO_NUMBER(DIVIDENDYIELD)          as dividend_yield,
+        TRY_TO_NUMBER(EPS)                    as eps,
+        TRY_TO_NUMBER(REVENUEPERSHARETTM)     as revenue_per_share_ttm,
+        TRY_TO_NUMBER(PROFITMARGIN)           as profit_margin,
+        TRY_TO_NUMBER(OPERATINGMARGINTTM)     as operating_margin_ttm,
+        TRY_TO_NUMBER(RETURNONASSETS)         as return_on_assets,
+        TRY_TO_NUMBER(RETURNONEQUITY)         as return_on_equity,
+        TRY_TO_NUMBER(REVENUETTM)             as revenue_ttm,
+        TRY_TO_NUMBER(GROSSPROFITTTM)         as gross_profit_ttm,
+        TRY_TO_NUMBER(DILUTEDEPSTTM)          as diluted_eps_ttm,
+        TRY_TO_NUMBER(QUARTERLYEARNINGSGROWTHYOY)  as quarterly_earnings_growth_yoy,
+        TRY_TO_NUMBER(QUARTERLYREVENUEGROWTHYOY)   as quarterly_revenue_growth_yoy,
+        TRY_TO_NUMBER(ANALYSTTARGETPRICE)     as analyst_target_price,
+        TRY_TO_NUMBER(TRAILINGPE)             as trailing_pe,
+        TRY_TO_NUMBER(FORWARDPE)              as forward_pe,
+        TRY_TO_NUMBER(PRICETOSALESTTM)        as price_to_sales_ttm,
+        TRY_TO_NUMBER(PRICETOBOOKRATIO)       as price_to_book_ratio,
+        TRY_TO_NUMBER(EVTOREVENUE)            as ev_to_revenue,
+        TRY_TO_NUMBER(EVTOEBITDA)             as ev_to_ebitda,
+        TRY_TO_NUMBER(BETA)                   as beta,
+        TRY_TO_NUMBER(WEEK52HIGH)             as week_52_high,
+        TRY_TO_NUMBER(WEEK52LOW)              as week_52_low,
+        TRY_TO_NUMBER(DAY50MOVINGAVERAGE)     as day_50_ma,
+        TRY_TO_NUMBER(DAY200MOVINGAVERAGE)    as day_200_ma,
+        TRY_TO_NUMBER(SHARESOUTSTANDING)      as shares_outstanding,
 
-        TRY_TO_DATE(dividenddate)              AS dividend_date,
-        TRY_TO_DATE(exdividenddate)            AS ex_dividend_date,
+        -- DATES
+        TRY_TO_DATE(DIVIDENDDATE)             as dividend_date,
+        TRY_TO_DATE(EXDIVIDENDDATE)           as ex_dividend_date,
 
-        -- script + pipeline metadata
-        ticker,
-        ingest_timestamp,
-        metadata$filename                    AS source_filename,
-        load_time,
+        -- METADATA
+        TICKER,
+        INGEST_TIMESTAMP,
+        METADATA$FILENAME as source_filename,
+        LOAD_TIME,
 
-        -- load date (for partitioning / snapshots)
-        TO_DATE(ingest_timestamp)             AS load_date
+        TO_DATE(INGEST_TIMESTAMP) as load_date
 
-    FROM {{ source('stock_data', 'company_overview_raw') }}
-
+    from {{ source('stock_data', 'company_overview_raw') }}
 )
 
-SELECT
-    *
-FROM source_data
+select * from raw
