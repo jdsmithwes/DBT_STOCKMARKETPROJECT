b5aac652074f322759bf8df34dc78e008e285243
jdsmithwes <jdsmith1906@gmail.com>
2025-11-16 19:43:50 -0500

    updated staging ro make aliases all caps

--- dbt_stockmarketproject/models/intermediate/int_growth_factors.sql
+++ dbt_stockmarketproject/models/intermediate/int_growth_factors.sql
@@ -1,41 +1,13 @@
-WITH fundamentals AS (
-    SELECT
-        ticker,
-        revenue_ttm,
-        gross_profit_ttm,
-        eps,
-        quarterly_earnings_growth_yoy,
-        quarterly_revenue_growth_yoy
-    FROM {{ ref('stg_stockoverview') }}
-),
+{{ config(materialized='view') }}
 
-momentum AS (
-    SELECT
-        stock_ticker,
-        one_month_return,
-        three_month_return,
-        six_month_return
-    FROM {{ ref('int_price_momentum') }}
-)
+select
+    TICKER,
+    REVENUE_TTM,
+    GROSS_PROFIT_TTM,
+    QUARTERLY_EARNINGS_GROWTH_YOY,
+    QUARTERLY_REVENUE_GROWTH_YOY,
+    REVENUE_PER_SHARE_TTM,
+    DILUTED_EPS_TTM,
+    LOAD_TIME
 
-SELECT
-    f.ticker,
-    f.revenue_ttm,
-    f.gross_profit_ttm,
-    f.eps,
-    f.quarterly_earnings_growth_yoy,
-    f.quarterly_revenue_growth_yoy,
-
-    m.one_month_return,
-    m.three_month_return,
-    m.six_month_return,
-
-    (
-        COALESCE(f.quarterly_revenue_growth_yoy, 0) +
-        COALESCE(f.quarterly_earnings_growth_yoy, 0) +
-        0.5 * COALESCE(m.three_month_return, 0)
-    ) AS blended_growth_score
-
-FROM fundamentals f
-LEFT JOIN momentum m
-    ON f.ticker = m.stock_ticker
+from {{ ref('stg_stockoverview') }}
--- dbt_stockmarketproject/models/staging/stg_stockoverview.sql
+++ dbt_stockmarketproject/models/staging/stg_stockoverview.sql
@@ -1,47 +1,60 @@
-with src as (
+{{ config(materialized='view') }}
+
+with source as (
 
     select
-        raw,
-        metadata$filename as file_name,
-        load_time
+        RAW:"Symbol"::string              as TICKER,
+        RAW:"AssetType"::string          as ASSETTYPE,
+        RAW:"Name"::string               as NAME,
+        RAW:"Description"::string        as DESCRIPTION,
+        RAW:"CIK"::string                as CIK,
+        RAW:"Exchange"::string           as EXCHANGE,
+        RAW:"Currency"::string           as CURRENCY,
+        RAW:"Country"::string            as COUNTRY,
+        RAW:"Sector"::string             as SECTOR,
+        RAW:"Industry"::string           as INDUSTRY,
+        RAW:"Address"::string            as ADDRESS,
+        RAW:"FiscalYearEnd"::string      as FISCALYEAREND,
+        RAW:"LatestQuarter"::string      as LATESTQUARTER,
+        RAW:"MarketCapitalization"::string as MARKETCAPITALIZATION,
+        RAW:"EBITDA"::string             as EBITDA,
+        RAW:"PERatio"::string            as PERATIO,
+        RAW:"PEGRatio"::string           as PEGRATIO,
+        RAW:"BookValue"::string          as BOOKVALUE,
+        RAW:"DividendPerShare"::string   as DIVIDENDPERSHARE,
+        RAW:"DividendYield"::string      as DIVIDENDYIELD,
+        RAW:"EPS"::string                as EPS,
+        RAW:"RevenuePerShareTTM"::string as REVENUEPERSHARETTM,
+        RAW:"ProfitMargin"::string       as PROFITMARGIN,
+        RAW:"OperatingMarginTTM"::string as OPERATINGMARGINTTM,
+        RAW:"ReturnOnAssetsTTM"::string  as RETURNONASSETS,
+        RAW:"ReturnOnEquityTTM"::string  as RETURNONEQUITY,
+        RAW:"RevenueTTM"::string         as REVENUETTM,
+        RAW:"GrossProfitTTM"::string     as GROSSPROFITTTM,
+        RAW:"DilutedEPSTTM"::string      as DILUTEDEPSTTM,
+        RAW:"QuarterlyEarningsGrowthYOY"::string as QUARTERLYEARNINGSGROWTHYOY,
+        RAW:"QuarterlyRevenueGrowthYOY"::string  as QUARTERLYREVENUEGROWTHYOY,
+        RAW:"AnalystTargetPrice"::string         as ANALYSTTARGETPRICE,
+        RAW:"TrailingPE"::string                 as TRAILINGPE,
+        RAW:"ForwardPE"::string                  as FORWARDPE,
+        RAW:"PriceToSalesRatioTTM"::string       as PRICETOSALESTTM,
+        RAW:"PriceToBookRatio"::string           as PRICETOBOOKRATIO,
+        RAW:"EVToRevenue"::string                as EVTOREVENUE,
+        RAW:"EVToEBITDA"::string                 as EVTOEBITDA,
+        RAW:"Beta"::string                       as BETA,
+        RAW:"52WeekHigh"::string                 as WEEK52HIGH,
+        RAW:"52WeekLow"::string                  as WEEK52LOW,
+        RAW:"50DayMovingAverage"::string         as DAY50MOVINGAVERAGE,
+        RAW:"200DayMovingAverage"::string        as DAY200MOVINGAVERAGE,
+        RAW:"SharesOutstanding"::string          as SHARESOUTSTANDING,
+        RAW:"DividendDate"::string               as DIVIDENDDATE,
+        RAW:"ExDividendDate"::string             as EXDIVIDENDDATE,
+
+        METADATA$FILENAME as SOURCE_FILE,
+        LOAD_TIME          as LOAD_TIME
+
     from {{ source('stock_data', 'company_overview_json_raw') }}
-
-),
-
-parsed as (
-
-    select
-        raw:"Symbol"::string                           as symbol,
-        raw:"AssetType"::string                        as asset_type,
-        raw:"Name"::string                             as company_name,
-        raw:"Description"::string                      as description,
-        raw:"CIK"::string                              as cik,
-        raw:"Exchange"::string                         as exchange,
-        raw:"Currency"::string                         as currency,
-        raw:"Country"::string                          as country,
-        raw:"Sector"::string                           as sector,
-        raw:"Industry"::string                         as industry,
-        
-        raw:"MarketCapitalization"::number             as market_cap,
-        raw:"EBITDA"::number                           as ebitda,
-        raw:"PERatio"::float                           as pe_ratio,
-        raw:"PEGRatio"::float                          as peg_ratio,
-        raw:"BookValue"::float                         as book_value,
-        raw:"DividendPerShare"::float                  as dividend_per_share,
-        raw:"DividendYield"::float                     as dividend_yield,
-        raw:"EPS"::float                               as eps,
-        raw:"RevenueTTM"::number                       as revenue_ttm,
-        raw:"GrossProfitTTM"::number                   as gross_profit_ttm,
-        raw:"AnalystTargetPrice"::float                as analyst_target_price,
-        raw:"52WeekHigh"::float                        as fifty_two_week_high,
-        raw:"52WeekLow"::float                         as fifty_two_week_low,
-        raw:"DividendDate"::string                     as dividend_date,
-        raw:"ExDividendDate"::string                   as ex_dividend_date,
-
-        file_name,
-        load_time
-    from src
 
 )
 
-select * from parsed
+select * from source
