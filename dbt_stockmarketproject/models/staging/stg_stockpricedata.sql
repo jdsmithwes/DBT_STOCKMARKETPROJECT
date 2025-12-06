badb3e7a0aa6e97f5543ea0fc78ce2827e4f03bf
jdsmithwes <jdsmith1906@gmail.com>
2025-12-06 10:44:50 -0500

    implemented incremental loading for daily stock data

--- dbt_stockmarketproject/models/staging/stg_stockpricedata.sql
+++ dbt_stockmarketproject/models/staging/stg_stockpricedata.sql
@@ -1,3 +1,8 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = ['stock_ticker', 'trading_date']
+) }}
 
 with stockdata as (
     select
@@ -10,11 +15,25 @@ with stockdata as (
         VOLUME as trading_volume,
         DIVIDEND_AMOUNT as dividend_amount,
         SPLIT_COEFFICIENT as split_coefficient,
-        ADJUSTED_CLOSE as adjusted_close_price
-        
+        ADJUSTED_CLOSE as adjusted_close_price,
+        LOAD_TIME
+    from {{ source('stock_data', 'stock_price_data_raw') }}
 
-        from {{ source('stock_data', 'stock_price_data_raw') }}
+    {% if is_incremental() %}
+        -- Load only rows that arrived after the latest load_time already processed
+        where LOAD_TIME > (select max(LOAD_TIME) from {{ this }})
+    {% endif %}
+)
 
-    )
-
-select * from stockdata
+select
+    stock_ticker,
+    trading_date,
+    open_price,
+    interday_high_price,
+    interday_low_price,
+    close_price,
+    trading_volume,
+    dividend_amount,
+    split_coefficient,
+    adjusted_close_price
+from stockdata
